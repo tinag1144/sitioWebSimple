b0ffbf426e158fe5a5fde1f5f430f83bf038d99c
unknown <sogazelaznog@gmail.com>
2026-03-12 10:52:09 -0300

    Realicé la segunda implementación utilizando express

---
+++ PROYECTOWEB/implementacion2/app.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+const PORT = 3000
+
+app.use(express.static("public"))
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:3000`)
+})
